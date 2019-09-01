a126a47dad657f41e0a3649230f5bcd567896b2e
Lennart Elbe <lenni.elbe@gmail.com>
2019-09-01 13:29:13 +0200

    improved security of computeOnServer.php by creating a special tmp and then deleting afterwards. Improved README

--- README.md
+++ README.md
@@ -2,19 +2,41 @@
 The aim of this project is to compare how python scripts are run clientside
 using Skulpt and in a serverside Docker container.
 
-In order to start this project do the following
+## Integration into ILIAS
+
+The Ilias web-server installation uses php (v5.6). We can use this capability
+to start one python job inside a docker container for each python script 
+the student evaluates. After each call, the output is returned to the client
+browser and the container running on the Ilias server is destroyed. This 
+ensures that file changes of one user do not propagate to other users. We will
+also make sure that the user permission used inside our docker container are
+more restrictive so that the python scripts cannot do harm.
+
+## How to build the Python 3.7 Docker container
+
+The only software that the docker container needs is python (v3.7). We can 
+therefore use a minimal docker image like ubuntu:18.04 to provide the python
+libraries required for all student scripts.
+
+The documentation on how to build the docker container is in 'Dockerfile'. 
+Build a local version of the container with:
+
+```
+docker build -t python37 -f Dockerfile .
+```
+After the build your python37 docker images should be about 125mb.
+
+## Debug instructions
+In order to be able to test this project without Ilias you can start a php-based
+webserver on your laptop or workstation:
 ```
 php -S localhost:8000
 ```
-And connect to
+
+Connect to
+```
 http://localhost:8000
 ```
-open
-```
-
-## How to build the Docker container
-
-In order to build execute the following in bash
-```
-docker build -t phppython37 -f php_Dockerfile .
-```
+and you should see a test page that includes the application. Enter a python
+script on the left and press the "Compute" button. The resulting text should
+appear on the right.
--- php/computeOnServer.php
+++ php/computeOnServer.php
@@ -1,10 +1,31 @@
 <?php
 
+// We want to create a python file with the content from the current
+// call. Lets create a temporary directory to store that python file.
+// After we run the file we delete the directory again. We only return
+// the output of the python run.
+
+function tempdir() {
+    $tempfile=tempnam(sys_get_temp_dir(),'');
+    // you might want to reconsider this line when using this snippet.
+    // it "could" clash with an existing directory and this line will
+    // try to delete the existing one. Handle with caution.
+    if (file_exists($tempfile)) { unlink($tempfile); }
+    mkdir($tempfile);
+    if (is_dir($tempfile)) { 
+        return $tempfile; 
+    }
+}
+
+$tmpdir = tempdir();
+
 $text = "";
-if (isset($_GET['code'])){
+if (isset($_GET['code'])) {
+    // The text from the user is not evaluted, only stored in a text file
+    // for python.
     $text = $_GET['code'];
 }
-file_put_contents('/tmp/code.py', $text);
+file_put_contents($tmpdir . '/code.py', $text);
 
 // now run the code locally on a docker images (for example)
 $output = array();
@@ -12,7 +33,21 @@ $output = array();
 // custom image
 //$ok = exec( 'docker run --rm -it -v /tmp/:/data python37 /bin/bash -c "/usr/bin/python3.7 /data/code.py"', $output);
 // php image
-$ok = exec( 'docker run --rm -it -v /tmp/:/data phppython37 /bin/bash -c "/usr/bin/python3.7 /data/code.py"', $output);
+// We run a docker per script evaluation and throw away the folder alterwards.
+$ok = exec( 'docker run --rm -it -v '.$tmpdir.':/data phppython37 /bin/bash -c "/usr/bin/python3.7 /data/code.py"', $output);
 
 echo(json_encode(array( "output" => $output )));
+
+// delete the folder again (and all files inside)
+function delTree($dir) { 
+   $files = array_diff(scandir($dir), array('.','..')); 
+    foreach ($files as $file) { 
+      (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
+    } 
+    return rmdir($dir); 
+}
+
+// only delete the temporary directory (server script, should be secure)
+delTree($tmpdir);
+
 ?>
